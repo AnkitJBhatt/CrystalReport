bba2ec0884b9653ede9615d51200595e0afd1ac2
Ankit Bhatt <ankit.bhatt@sayalife.in>
2025-04-17 09:24:02 +0530

    display report done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.Windows;
 using System.Data;
 using System.Data.SqlClient;
-using CrystalReportDemo.Forms;
 using System;
 
 namespace CrystalReportDemo
@@ -13,16 +12,8 @@ namespace CrystalReportDemo
         public MainWindow()
         {
             InitializeComponent();
+           
         }
-
-        //private void btnGrid_Click(object sender, RoutedEventArgs e)
-        //{
-        //    SqlCommand cmd = new SqlCommand("SELECT TOP (100) * FROM AlarmHistory", con);
-        //    DataTable dt = new DataTable();
-        //    dt.Load(cmd.ExecuteReader());
-        //    dataGridViewTest.ItemsSource = dt.DefaultView;
-        //    con.Close();
-        //}
 
         private void btnViewer_Click(object sender, RoutedEventArgs e)
         {
@@ -30,10 +21,8 @@ namespace CrystalReportDemo
             {
                 SqlConnection con = new SqlConnection("Data Source=DESKTOP-IA46C6P\\SQLEXPRESS;Initial Catalog=ArkaPRP84_Alarm;User ID=sa;Password=Test#1234");
                 con.Open();
-                // SQL Query: Get user action data ordered by user name and action date
-                string query = @"
-            SELECT UA.[Actions], UA.[Comment], UA.[OldValue], UA.[NewValue], UA.[ActionType], UA.[ActionDateAndTime], U.FullName FROM [ArkaPRP84_UserAudit].[dbo].[UserAction] UA 
-            INNER JOIN [ArkaPRP84_UserAudit].[dbo].[User] U ON U.Id = UA.UserId WHERE U.Id in(141,10140,10141) ORDER BY U.FullName, UA.ActionType;";
+                string query = @"SELECT UA.[Actions], UA.[Comment], UA.[OldValue], UA.[NewValue], UA.[ActionType], UA.[ActionDateAndTime], U.FullName FROM [ArkaPRP84_UserAudit].[dbo].[UserAction] UA 
+                                INNER JOIN [ArkaPRP84_UserAudit].[dbo].[User] U ON U.Id = UA.UserId WHERE U.Id in(141,10140,10141) ORDER BY U.FullName, UA.ActionType;";
 
                 SqlCommand cmd = new SqlCommand(query, con);
                 SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
@@ -41,18 +30,14 @@ namespace CrystalReportDemo
 
                 sqlDataAdapter.Fill(ds, "Alarm");
 
-                // Check if any data is returned
                 if (ds.Tables["Alarm"].Rows.Count == 0)
                 {
                     MessageBox.Show("No data found.");
                     return;
                 }
 
-                // Create Crystal Report instance and bind data
-                CrystalReport crystalReport = new CrystalReport(); // Replace with your actual report class name
+                CrystalReport crystalReport = new CrystalReport(); 
                 crystalReport.SetDataSource(ds.Tables["Alarm"]);
-
-                // Assign report to the WPF viewer
                 crystalReportViewer.ViewerCore.ReportSource = crystalReport;
             }
             catch (Exception ex)
@@ -113,11 +98,8 @@ GROUP BY DateTime  ORDER BY OperationId, DateTime ASC;";
                             return;
                         }
 
-                        // Load the report
                         BatchParameterReport report = new BatchParameterReport();
                         report.SetDataSource(ds.Tables["ParameterLogTable"]);
-
-                        // Bind to the Crystal Reports viewer
                         crystalReportViewer2.ViewerCore.ReportSource = report;
                     }
                 }
@@ -126,32 +108,6 @@ GROUP BY DateTime  ORDER BY OperationId, DateTime ASC;";
             {
                 MessageBox.Show("Error: " + ex.Message);
             }
-
-
-
-
-            //DataTable dt = GetReportData();
-            //BatchParameterReport batchParameterReport = new BatchParameterReport(); // your .rpt file
-            //batchParameterReport.SetDataSource(dt);
-            //crystalReportViewer2.ViewerCore.ReportSource = batchParameterReport;
-
-        }
-
-        public DataTable GetReportData()
-        {
-            SqlConnection con2 = new SqlConnection("Data Source=DESKTOP-IA46C6P\\SQLEXPRESS;Initial Catalog=CrystalReportDemo;User ID=sa;Password=Test#1234");
-            var dt = new DataTable();
-
-            using (SqlCommand cmd = new SqlCommand("usp_GetBatchParameterReport", con2))
-            {
-                cmd.CommandType = CommandType.StoredProcedure;
-                con2.Open();
-                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
-                {
-                    da.Fill(dt);
-                }
-            }
-            return dt;
         }
     }
 }
